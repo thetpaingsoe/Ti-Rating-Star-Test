59a9eae9b3ecbcb517fa29d5d31d8147ea1ba16b
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2014-11-08 02:01:29 +0800

    added module, added test code, added vertical star rating function

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,34 @@ function doClick(e) {
     alert($.label.text);
 }
 
+var doRating = function(rating){
+	
+	alert("Rating : "+ rating);
+	
+	$.starwidget.setRating(rating);
+	
+};
+
+
+var showRating = function() {
+	
+	// get the current rating:
+	var rating = $.starwidget.getRating();
+	
+	alert(" Rating Star : " + rating);
+	
+};
+
+$.index.addEventListener('open', function(){
+	
+	$.starwidget.init(doRating); 
+	
+	// start another star rating widget
+	$.starwidget2.init(doRating);
+	
+	// make vertical star rating 
+	$.starwidget3.init(doRating);
+	$.starwidget3.makeVertical();
+});
+
 $.index.open();
